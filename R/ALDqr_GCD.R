c72af89840588b8074df7cc972daf549d740cc25
WangWenjing <wenjingwang1990@126.com>
2016-11-16 17:57:42 +1100

    revise function ALDqr_GCD

--- R/ALDqr_GCD.R
+++ R/ALDqr_GCD.R
@@ -1,12 +1,9 @@
 ALDqr_GCD <- function(y, x = NLLL, tau = NULL,
-               error = 1e-06, iter = 2000)
+                      error = 1e-06, iter = 2000)
 {
+  p <- ncol(x)
+  n <- length(y)
   theta <- EM.qr(y, x, tau, error, iter)$theta
-  theta_i <- ALDqr_case_deletion(y, x, tau, error, iter)
-  beta_i <- theta_i$beta_i
-  sigma_i <- theta_i$sigma_i
-  xM <- c(sqrt(vchpN)) * x
-  suma1 <- t(xM) %*% (xM)
   beta_qr <- theta[1:p, ]
   sigma_qr <- theta[p+1]
   taup2 <- (2/(tau * (1 - tau)))
@@ -16,41 +13,38 @@ ALDqr_GCD <- function(y, x = NLLL, tau = NULL,
   muc <- y - x %*% beta_qr
   vchpN <- besselK(sqrt(delta2 * gamma2), 0.5 - 1)/(besselK(sqrt(delta2 *
                   gamma2), 0.5)) * (sqrt(delta2 / gamma2))^(-1)
+
   vchp1 <- besselK(sqrt(delta2 * gamma2), 0.5 + 1)/(besselK(sqrt(delta2 *
                   gamma2), 0.5)) * (sqrt(delta2 / gamma2))
-  E1 <- 1: length(y) %>%
-    map(function(i){
-      suma2 <- x[-i, ] * c(vchpN[-i] * c(y[-i] - x[-i, ] %*% beta_qr) - thep)
-      E1_i <- apply(suma2, 2, sum)/(taup2)
-      return(E1_i)
-    })
-  E1 <- simplify2array(E1)
-  E2 <- 1: length(y) %>%
+  E1 <- matrix(0, nrow = p, ncol = n)
+  for(i in 1:n){
+    suma2 <- x[-i,] * c(vchpN[-i] * (y[-i] - x[-i,] %*% beta_qr) - thep)
+    E1[,i] <- apply(suma2, 2, sum)/(taup2)
+  }
+  E2 <- 1: n %>%
     map(function(i) {
       muc_i <- y[-i] - x[-i, ]%*%beta_qr
-      E2_i <- sum(3*sigma_qr*rep(1, length(y)-1) - vchpN[-i] * muc_i^2/taup2 - 2 * muc_i * thep + vchp1[-i] *
-                    (thep^2 + 2 * taup2))
-      return(E2_i)
+      sum(3*sigma_qr - (vchpN[-i] * muc_i^2 -
+                          2 * muc_i * thep + vchp1[-i] *(thep^2 + 2 * taup2))/taup2)
     })
   E2 <- simplify2array(E2)
   Q1_beta <- E1/sigma_qr
   Q1_sigma <- -E2/(2*sigma_qr^2)
-  Q2_beta <- -solve(suma1)/ (sigma_qr * taup2)
-  Q2_sigma <- 3/(4*sigma_qr^2) - sum((vchpN*muc^2-2*muc*thep +
-              vchp1*(thep^2 + 2*taup2)))/(2*sigma_qr^3*taup2)
-  GCD_beta <-  1:length(y) %>%
+  xM <- c(sqrt(vchpN)) * x
+  suma1 <- t(xM) %*% (xM)
+  Q2_beta <- -(suma1)/(sigma_qr * taup2)
+  Q2_sigma <- 3/(2*sigma_qr^2) - sum((vchpN*muc^2-2*muc*thep +
+                                        vchp1*(thep^2 + 2*taup2)))/(sigma_qr^3*taup2)
+  GCD_beta <- 1:n %>%
     map(function(i){
-       c(Q1_beta[,i]) %*% solve(Q2_beta) %*% matrix(Q1_beta[,i], ncol = 1)
+      c(Q1_beta[,i]) %*% solve(-Q2_beta) %*% matrix(Q1_beta[,i], ncol = 1)
     })
-
-   GCD_beta <- simplify2array(GCD_beta)
-
-  GCD_sigma <- 1:length(y) %>%
+  GCD_beta <- simplify2array(GCD_beta)
+  GCD_sigma <- 1:n %>%
     map(function(i) {
-      c(Q1_sigma[i])*solve(Q2_sigma)*Q1_sigma[i]
+      Q1_sigma[i]*solve(-Q2_sigma)*Q1_sigma[i]
     })
   GCD_sigma <- simplify2array(GCD_sigma)
-  return(list(GCD_beta = GCD_beta, GCD_sigma = GCD_sigma))
+  GCD <- GCD_beta + GCD_sigma
+  return(GCD)
 }
-
-ALDqr_GCD(y, x, tau, error = 1e-06, iter = 2000)
