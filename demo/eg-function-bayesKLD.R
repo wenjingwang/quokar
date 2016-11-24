7a47f2b1a65d66275d2f23d1e7c6acb73018a935
WangWenjing <wenjingwang1990@126.com>
2016-11-24 14:17:29 +1100

    revise example for function bayesKLD

--- demo/eg-function-bayesKLD.R
+++ demo/eg-function-bayesKLD.R
@@ -15,52 +15,19 @@ y <- ais$BMI
 sexInd <-(ais$Sex == 1) + 0
 x <- cbind(1, ais$LBM, sexInd)
 
-ais_bayes <- bayesQR(y ~ x -1 , quantile = 0.5, alasso = FALSE, ndraw = 10000, prior = NULL)
+ais_bayes <- bayesQR(y ~ x - 1 , quantile = 0.5, alasso = FALSE, ndraw = 10000, prior = NULL)
 ais_bayes_beta <- summary(ais_bayes)[[1]]$betadraw[, 1]
 ais_bayes_sigma <- summary(ais_bayes)[[1]]$sigmadraw[, 1]
 
-
 tau <- 0.5
+M = 1000
+n <- length(y)
 beta <- ais_bayes_beta
 sigma <- ais_bayes_sigma
-taup2 <- (2/(tau * (1 - tau)))
-theta <- (1 - 2 * tau) / (tau * (1 - tau))
-n <- length(y)
 
-delta <- sqrt((y - x %*% beta)^2/(sigma * taup2))
-gamma <- sqrt(theta^2/(sigma*taup2) + 2/sigma)
+KLD <- bayesKLD(y, x, beta, sigma, tau, M)
 
-M <- 1000
-nu_dens <- matrix(0, nrow = M, ncol = n)
-
-for(i in 1:n) {
-  nu_dens[,i] <- rgig(M, 1/2, delta[i], gamma)
-}
-
-
-KLS <- matrix(0, nrow = n, ncol = n-1)
-for(i in 1:n) {
-  nu1 <- nu_dens[, -i]
-  hi <- density(nu_dens[, i], kernel = "gaussian")$bw
-  upper_x <- max(nu_dens[, i])
-  lower_x <- min(nu_dens[, i])
-  for(j in 1:(n-1)) {
-    hj <- density(nu1[, j], kernel = "gaussian")$bw
-    func <- function(xx) {
-      log((1/(M*sqrt(2*pi))*sum(exp(-1/2*((xx-nu_dens[,i])/hi)^2)))/
-            (1/(M*sqrt(2*pi))*sum(exp(-1/2*((xx-nu1[,j])/hj)^2))))*
-        (1/(M*sqrt(2*pi))*sum(exp(-1/2*((xx-nu_dens[,i])/hi)^2)))
-    }
-    KLS[i,j] <- (upper_x - lower_x)*(func(upper_x)+func(lower_x))/2
-  }
-}
-
-KLD <- apply(KLS, 1, mean)
-for(i in 1:n) {
-  if (KLD[i] == Inf) {KLD[i] = 1}
-}
-
-case <- 1:202
+case <- 1:n
 ais_bayes_data <- data.frame(case = case, KLD = KLD)
 
 ggplot(ais_bayes_data, aes(x = case, y = KLD)) +
