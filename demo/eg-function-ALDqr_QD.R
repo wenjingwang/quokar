6b6de41b453aa6e25d219bd743b869285848a1da
WangWenjing <wenjingwang1990@126.com>
2016-12-05 09:54:24 +1100

    revise example ALDqr_QD

--- demo/eg-function-ALDqr_QD.R
+++ demo/eg-function-ALDqr_QD.R
@@ -3,42 +3,26 @@ library(ggplot2)
 library(magrittr)
 library(purrr)
 library(gridExtra)
+library(qr)
 
 data(ais)
 y <- ais$BMI
 sexInd <-(ais$Sex == 1) + 0
 x <- cbind(1, ais$LBM, sexInd)
 
-tau <- c(0.1, 0.25, 0.5, 0.9)
+tau <- c(0.1, 0.2, 0.5, 0.9)
 n <- length(y)
 case <- 1: n
 
-QD <- ALDqr_QD(y, x, tau[1], error = 1e-06, iter = 2000)
-data_QD <- data.frame(case = case, QD = QD)
-p1 <- ggplot(data_QD, aes(x = case, y = QD)) +
-  geom_point(colour = "darkblue") +
-  geom_text(data = subset(data_QD, QD > 1),
-            aes(case, QD+0.1, label = case))
+plots <- list()
+for(i in seq_along(tau)){
+  QD <- ALDqr_QD(y, x, tau[i], error = 1e-06, iter = 2000)
+  data_QD <- data.frame(case = case, QD = QD)
 
-QD <- ALDqr_QD(y, x, tau[2], error = 1e-06, iter = 2000)
-data_QD <- data.frame(case = case, QD = QD)
-p2 <- ggplot(data_QD, aes(x = case, y = QD)) +
-  geom_point(colour = "darkblue") +
-  geom_text(data = subset(data_QD, QD > 1),
-            aes(case, QD+0.1, label = case))
+  plots[[i]] <- ggplot(data_QD, aes(x = case, y = QD)) +
+    geom_point(colour = "darkblue") +
+    geom_text(data = subset(data_QD, QD > 1),
+              aes(case, QD+0.1, label = case))
+}
+grid.arrange(grobs=plots)
 
-QD <- ALDqr_QD(y, x, tau[3], error = 1e-06, iter = 2000)
-data_QD <- data.frame(case = case, QD = QD)
-p3 <- ggplot(data_QD, aes(x = case, y = QD)) +
-  geom_point(colour = "darkblue") +
-  geom_text(data = subset(data_QD, QD > 0.5),
-            aes(case, QD+0.1, label = case))
-
-QD <- ALDqr_QD(y, x, tau[4], error = 1e-06, iter = 2000)
-data_QD <- data.frame(case = case, QD = QD)
-p4 <- ggplot(data_QD, aes(x = case, y = QD)) +
-  geom_point(colour = "darkblue") +
-  geom_text(data = subset(data_QD, QD > 1),
-            aes(case, QD+0.1, label = case))
-
-grid.arrange(p1, p2, p3, p4, ncol = 2)
