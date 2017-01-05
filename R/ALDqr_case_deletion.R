9c7c073e477b77bf4c7b70592b524a1247decdff
Wenjing Wang <wenjingwang1990@126.com>
2017-01-05 23:38:11 +1100

    Update ALDqr_case_deletion.R

--- R/ALDqr_case_deletion.R
+++ R/ALDqr_case_deletion.R
@@ -31,7 +31,8 @@ ALDqr_case_deletion <- function(y, x, tau, error, iter)
   muc <- y - x %*% beta_qr
   vchp1 <- besselK(sqrt(delta2 * gamma2), 0.5 + 1)/(besselK(sqrt(delta2 *
                       gamma2), 0.5)) * (sqrt(delta2 / gamma2))
-  vchpN <- vchp1^(-1)
+  vchpN <- besselK(sqrt(delta2 * gamma2), 0.5 - 1)/(besselK(sqrt(delta2 *
+                      gamma2), 0.5)) * (sqrt(delta2 / gamma2))^(-1)
   E1 <- matrix(0, nrow = p, ncol = n)
   for(i in 1:n){
     suma2 <- x[-i,] * c(vchpN[-i] * (y[-i] - x[-i,] %*% beta_qr) - thep)
