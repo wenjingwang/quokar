eb17c60e3947a33ab1f9ca1d09a5a345a9807daf
WangWenjing <wenjingwang1990@126.com>
2017-01-17 23:12:08 +1100

    add document

--- R/ais.R
+++ R/ais.R
@@ -3,8 +3,26 @@
 #' @docType data
 #' @description Data on 102 male and 100 female athletes collected at the Austrialian
 #' Institute of Sports
+#' @format A data frame with 202 observations on the following 14 variables
+#' \describe{
+#' \item{Sex}{0 = male, 1 = female}
+#' \item{Ht}{height(cm)}
+#' \item{Wt}{weight{kg}}
+#' \item{LBM}{lean body mass}
+#' \item{RCC}{red cell count}
+#' \item{WCC}{white cell count}
+#' \item{Hc}{Hematocrit}
+#' \item{Hg}{Hemoglobin}
+#' \item{Ferr}{plasma ferritin concentration}
+#' \item{BMI}{body mass index, weight/(height)^2}
+#' \item{SSF}{sum of skin folds}
+#' \item{Bfat}{Percent body fat}
+#' \item{Label}{Case Lables}
+#' \item{Sport}{Sport}
+#' }
 #' @references S.Weisberg(2005). Applied Linear Regression, 3rd edition. New York,
 #' Section 6.4.
 #' @keywords data set
 #'
 NULL
+
