3d5d675d7fe88de7d47d5a7eb17a1d509b8e6d27
wenjingwang <wenjingwang1990@126.com>
2017-02-22 14:10:53 +1100

    revise the output of weighting matrix of fn

--- R/frame_fn_obs.R
+++ R/frame_fn_obs.R
@@ -58,27 +58,55 @@
 #' data(ais)
 #' tau <- c(0.1, 0.5, 0.9)
 #' object <- rq(BMI ~ LBM + Ht, data = ais, tau = tau, method = 'fn')
-#' fn <- frame_fn_obs(object, tau)
+#" fn <- frame_fn_obs(object, tau)
 #' ##For tau = 0.1, plot the observations used in quantile regression
 #' ##fitting based on interior point method
 #' fn1 <- fn[[1]]
-#' case <- 1: nrow(fn1)
+#' case <- 1: length(fn1)
 #' fn1 <- cbind(case, fn1)
-#' fn1_f <- gather(fn1, variable, value, -case, -tau_flag, -y)
-#' ggplot(fn1_f, aes(x = value, y = y)) +
-#' geom_point(colour = 'red') +
-#'    geom_text(aes(label = case)) +
-#'    facet_wrap(~ variable, scale = "free")
+#' m <- data.frame(y = ais$BMI, x1 = ais$LBM, x2 = ais$Ht, fn1)
+#' p <- length(attr(object$coefficients, "dimnames")[[1]])
+#' m_f <- m %>% gather(variable, value, -case, -fn1, -y)
+#' mf_a <- m_f %>%
+#'  group_by(variable) %>%
+#'  arrange(variable, desc(fn1)) %>%
+#'  filter(row_number() %in% 1:p )
+#' p1 <- ggplot(m_f, aes(x = value, y = y)) +
+#'  geom_point(alpha = 0.1) +
+#'  geom_point(data = mf_a, size = 3) +
+#'  facet_wrap(~variable, scale = "free_x")
 #' ## For tau = 0.5, plot the observations used in quantile regression
 #' ##fitting based on interior point method
 #' fn2 <- fn[[2]]
-#' case <- 1:nrow(fn2)
+#' case <- 1: length(fn2)
 #' fn2 <- cbind(case, fn2)
-#' fn2_f <- gather(fn2, variable, value, -case, -tau_flag, -y)
-#' ggplot(fn2_f, aes(x = value, y = y)) +
-#'    geom_point(colour = 'red') +
-#'    geom_text(aes(label = case)) +
-#'    facet_wrap(~variable, scale = 'free')
+#' m <- data.frame(y = ais$BMI, x1 = ais$LBM, x2 = ais$Ht, fn2)
+#' p <- length(attr(object$coefficients, "dimnames")[[1]])
+#' m_f <- m %>% gather(variable, value, -case, -fn2, -y)
+#' mf_a <- m_f %>%
+#'    group_by(variable) %>%
+#'    arrange(variable, desc(fn2)) %>%
+#'    filter(row_number() %in% 1:p )
+#' p2 <- ggplot(m_f, aes(x = value, y = y)) +
+#'    geom_point(alpha = 0.1) +
+#'    geom_point(data = mf_a, size = 3) +
+#'    facet_wrap(~variable, scale = "free_x")
+#' ## For tau = 0.9
+#' fn3 <- fn[[3]]
+#' case <- 1: length(fn3)
+#' fn3 <- cbind(case, fn3)
+#' m <- data.frame(y = ais$BMI, x1 = ais$LBM, x2 = ais$Ht, fn3)
+#' p <- length(attr(object$coefficients, "dimnames")[[1]])
+#' m_f <- m %>% gather(variable, value, -case, -fn3, -y)
+#' mf_a <- m_f %>%
+#'   group_by(variable) %>%
+#'   arrange(variable, desc(fn3)) %>%
+#'   filter(row_number() %in% 1:p )
+#' p3 <- ggplot(m_f, aes(x = value, y = y)) +
+#'   geom_point(alpha = 0.1) +
+#'   geom_point(data = mf_a, size = 3) +
+#'   facet_wrap(~variable, scale = "free_x")
+#' grid.arrange(p1, p2, p3, ncol = 1)
 
 frame_fn_obs <- function(object, tau){
     y <- matrix(object$model[,1], ncol = 1)
@@ -99,6 +127,7 @@ frame_fn_obs <- function(object, tau){
     d <- rep(1, n)
     u <- rep(1, n)
     wn <- rep(0, 10 * n)
+    W <- matrix(0, nrow = )
     1:length(tau) %>%
         map(function(i){
             rhs <- (1 - tau[i]) * apply(x, 2, sum)
@@ -120,14 +149,17 @@ frame_fn_obs <- function(object, tau){
                                it.routine = double(50*p),
                                PACKAGE = "quokar")
             W <- z[[i]]$d
-            x <- as.matrix(object$model[, -1])
-            wx <- diag(sqrt(W)) %*% x
-            wy <- diag(sqrt(W)) %*% y
-            colnames(wx) <- paste('x', 1:ncol(x), sep='')
-            colnames(wy) <- 'y'
-            tau_flag <- paste('tau', tau[i], sep = '')
-            w_obs[[i]] <- data.frame(wy, wx)
-            w_obs[[i]] <- cbind(tau_flag, w_obs[[i]])
+            print(W)
+            #x <- as.matrix(object$model[, -1])
+            #wx <- diag(sqrt(W)) %*% x
+            #wy <- diag(sqrt(W)) %*% y
+            #colnames(wx) <- paste('x', 1:ncol(x), sep='')
+            #colnames(wy) <- 'y'
+            #tau_flag <- paste('tau', tau[i], sep = '')
+            #w_obs[[i]] <- data.frame(wy, wx)
+            #w_obs[[i]] <- cbind(tau_flag, w_obs[[i]])
         })
 }
+
+
 
