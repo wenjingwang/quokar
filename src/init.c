120ba497f5616ef23b111fe21465836b4e2d7310
Wenjing Wang <wenjingwang1990@126.com>
2017-11-07 22:43:35 +0800

    Create init.c

---
+++ src/init.c
@@ -0,0 +1,26 @@
+#include <R_ext/RS.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME:
+Check these declarations against the C/Fortran source code.
+*/
+
+
+
+/* .Fortran calls */
+
+extern void F77_NAME(rqbr)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(rqfnb)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_FortranMethodDef FortranEntries[] = {
+  {"rqbr",  (DL_FUNC) &F77_NAME(rqbr),  27},
+  {"rqfnb", (DL_FUNC) &F77_NAME(rqfnb), 14},
+  {NULL, NULL, 0}
+};
+
+void R_init_quantreg(DllInfo *dll)
+{
+  R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
